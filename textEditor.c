5a4c0b186be808028f029bd8162e07fdb857d44e
Boshtrat <boshtrat@gmail.com>
2023-05-28 13:30:49 +0200

    feat: Ctrl+Q to quit, Refactor keyboard input, clear the screen and reposition the cursor

--- textEditor.c
+++ textEditor.c
@@ -1,3 +1,5 @@
+/** includes **/
+
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -5,7 +7,15 @@
 #include <stdio.h>
 #include <errno.h>
 
+/** defines **/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/** data **/
+
 struct termios orig_termios;
+
+/** terminal **/
 
 void die(const char *s)
 {
@@ -58,25 +68,55 @@ void enableRawMode()
         die("tcsetattr");
 }
 
+char editorReadKey() 
+{
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        //Won't treat EAGAIN to make it work in Cygwin
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            die("read");
+    }
+    return c;
+}
+
+/** output **/
+
+void editorRefreshScreen()
+{
+    //x1b is the escape character
+    //Escape sequence command take arguments, here we use VT100 escape sequences
+    //<esc>[2J clears the entire screen
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    //Reposition the cursor : first row and first column
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/** input **/
+
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+
+    switch(c)
+    {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
+/** init **/
+
 int main() 
 {
     enableRawMode();
 
     while (1)
     {
-        char c = '\0';
-        //Won't treat EAGAIN to make it work in Cygwin
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-            die("read");
-
-        if (iscntrl(c))
-            printf("%d\r\n", c);
-        else
-            printf("%d ('%c')\r\n", c, c);
-
-        if (c == 'q')
-            break;
-
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
     return 0;
 }
