52a37ad0e5f58ecf160a4c83d6c5d86b8565cff3
Boshtrat <boshtrat@gmail.com>
2023-05-28 14:26:00 +0200

    feat: Welcome message when opening editor

--- textEditor.c
+++ textEditor.c
@@ -11,6 +11,8 @@
 
 /** defines **/
 
+#define EDITOR_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /** data **/
@@ -171,7 +173,25 @@ void editorDrawRows(struct abuf *ab)
     //Put tildes on the left, like Vim
     for (int y = 0; y < E.screenrows; y++)
     {
-        abAppend(ab, "~", 1);
+        if (y == E.screenrows / 3)
+        {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                "My Editor -- version %s", EDITOR_VERSION);
+            if (welcomelen > E.screencols)
+                welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding)
+            {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--)
+                abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        }
+        else
+            abAppend(ab, "~", 1);
 
         //(Erase In Line) erases the part of the line to the right of the cursor
         abAppend(ab, "\x1b[K", 3);
