dc2d5881ac68fe3321a9b6d27d8d3286f64e5450
Boshtrat <boshtrat@gmail.com>
2023-06-06 18:30:57 +0200

    feat: Enter key now adds a new line, so moves it

--- textEditor.c
+++ textEditor.c
@@ -283,11 +283,14 @@ void editorUpdateRow(erow *row)
     row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len)
+void editorInsertRow(int at, char *s, size_t len)
 {
+    if (at < 0 || at > E.numrows)
+        return;
+    
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
 
-    int at = E.numrows;
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
@@ -359,9 +362,26 @@ void editorInsertChar(int c)
 {
     //If the cursor is on the line line after the end of the file
     if (E.cy == E.numrows)
-        editorAppendRow("", 0);
+        editorInsertRow(E.numrows, "", 0);
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
+}
+
+void editorInsertNewline()
+{
+    if (E.cx == 0)
+        editorInsertRow(E.cy, "", 0);
+    else
+    {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
 }
 
 void editorDelChar()
@@ -428,7 +448,7 @@ void editorOpen(char *filename)
         while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
             linelen--;
 
-        editorAppendRow(line, linelen);
+        editorInsertRow(E.numrows, line, linelen);
     }
     free(line);
     fclose(fp);
@@ -693,7 +713,7 @@ void editorProcessKeypress()
     switch(c)
     {
         case '\r':
-            //TODO
+            editorInsertNewline();
             break;
 
         case CTRL_KEY('q'):
