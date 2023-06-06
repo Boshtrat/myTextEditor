61af1d7b2ca33429a2e1d141b21b2c4c61c5e4e1
Boshtrat <boshtrat@gmail.com>
2023-06-06 19:07:57 +0200

    feat: Incremental search, try to find after each keypress

--- textEditor.c
+++ textEditor.c
@@ -68,7 +68,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void( *callback)(char *, int));
 
 /** terminal **/
 
@@ -478,7 +478,7 @@ void editorSave()
 {
     if (E.filename == NULL)
     {
-        E.filename = editorPrompt("Save as: %s");
+        E.filename = editorPrompt("Save as: %s", NULL);
         if (E.filename == NULL)
         {
             editorSetStatusMessage("Save aborted");
@@ -512,11 +512,11 @@ void editorSave()
 
 /** find **/
 
-void editorFind()
+void editorFindCallback(char *query, int key)
 {
-    char *query = editorPrompt("Search: %s (ESC to cancel)");
-    if (query == NULL)
+    if (key == '\r' || key == '\x1b')
         return;
+    
 
     for (int i = 0; i < E.numrows; i++)
     {
@@ -530,8 +530,13 @@ void editorFind()
             break;
         }
     }
+}
 
-    free(query);
+void editorFind()
+{
+    char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+    if (query)
+        free(query);
 }
 
 /** append buffer **/
@@ -712,7 +717,7 @@ void editorSetStatusMessage(const char *fmt, ...)
 
 /** input **/
 
-char *editorPrompt(char *prompt)
+char *editorPrompt(char *prompt, void (*callback)(char *, int))
 {
     size_t bufsize = 128;
     char *buf = malloc(bufsize);
@@ -736,6 +741,8 @@ char *editorPrompt(char *prompt)
         else if (c == '\x1b')
         {
             editorSetStatusMessage("");
+            if (callback)
+                callback(buf, c);
             free(buf);
             return NULL;
         }
@@ -744,6 +751,8 @@ char *editorPrompt(char *prompt)
             if (buflen != 0)
             {
                 editorSetStatusMessage("");
+                if (callback)
+                    callback(buf, c);
                 return buf;
             }
         }
@@ -758,6 +767,8 @@ char *editorPrompt(char *prompt)
             buf[buflen++] = c;
             buf[buflen] = '\0';
         }
+        if (callback)
+            callback(buf, c);
     }
 }
 
