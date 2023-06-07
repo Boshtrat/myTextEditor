d2fe24bd82241a664b9b80fe84b9a06b1c069d27
Boshtrat <boshtrat@gmail.com>
2023-06-07 21:15:02 +0200

    feat: Colorful string (magenta)

--- textEditor.c
+++ textEditor.c
@@ -24,6 +24,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 enum editorKey {
     BACKSPACE = 127,
@@ -40,6 +41,7 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
+    HL_STRING,
     HL_NUMBER,
     HL_MATCH
 };
@@ -89,7 +91,7 @@ struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
-        HL_HIGHLIGHT_NUMBERS
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     },
 };
 
@@ -289,12 +291,43 @@ void editorUpdateSyntax(erow *row)
 
     //Considering the beginning of the line to be a separator
     int prev_sep = 1;
+    //Keeping track of whethter we are currently inside a string
+    int in_string = 0;
 
     int i = 0;
     while (i < row->rsize)
     {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS)
+        {
+            if (in_string)
+            {
+                row->hl[i] = HL_STRING;
+                if (c == '\\' && i + 1 < row->rsize)
+                {
+                    row->hl[i + 1] = HL_STRING;
+                    i += 2;
+                    continue;
+                }
+                //Check for closing quote
+                if (c == in_string)
+                    in_string = 0;
+                i++;
+                prev_sep = 1;
+                continue;
+            }
+            else {
+                if (c == '"' || c == '\'')
+                {
+                    in_string = c;
+                    row->hl[i] = HL_STRING;
+                    i++;
+                    continue;
+                }
+            }
+        }
         
         if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS)
         {
@@ -316,6 +349,7 @@ int editorSyntaxToColor(int hl)
 {
     switch(hl)
     {
+        case HL_STRING: return 35;
         case HL_NUMBER: return 31;
         case HL_MATCH: return 34;
         default: return 37;
