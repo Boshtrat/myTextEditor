066b981a631121880bb269751ecd6c52cd484547
Boshtrat <boshtrat@gmail.com>
2023-06-07 17:18:53 +0200

    feat: Restore syntax highlighting after seach

--- textEditor.c
+++ textEditor.c
@@ -552,6 +552,18 @@ void editorFindCallback(char *query, int key)
     static int last_match = -1;
     static int direction = 1;
 
+    //To know which line's hl needs to be restored
+    static int saved_hl_line;
+    //Dynamically allocated array which points to NULL when there is noting to restore
+    static char *saved_hl = NULL;
+
+    if (saved_hl)
+    {
+        memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+        free(saved_hl);
+        saved_hl = NULL;
+    }
+
     if (key == '\r' || key == '\x1b')
     {
         last_match = -1;
@@ -590,6 +602,10 @@ void editorFindCallback(char *query, int key)
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+
+            saved_hl_line = current;
+            saved_hl = malloc(row->rsize);
+            memcpy(saved_hl, row->hl, row->rsize);
 
             memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
