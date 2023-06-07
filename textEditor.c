dd64f9306f899675a7e754da74825aa33e3bcd85
Boshtrat <boshtrat@gmail.com>
2023-06-07 17:11:40 +0200

    refactor: Refactoring syntax highlighting for bigger syntax, printing escape characters around keyword, not at each character

--- textEditor.c
+++ textEditor.c
@@ -36,6 +36,11 @@ enum editorKey {
     PAGE_DOWN
 };
 
+enum editorHighlight {
+    HL_NORMAL = 0,
+    HL_NUMBER
+};
+
 /** data **/
 
 typedef struct erow {
@@ -43,6 +48,7 @@ typedef struct erow {
     int rsize;
     char *chars;
     char *render;
+    unsigned char *hl; //Highlight
 } erow;
 
 struct editorConfig {
@@ -241,6 +247,29 @@ int getWindowSize(int *rows, int *cols)
     }
 }
 
+/** syntax highlighting **/
+
+void editorUpdateSyntax(erow *row)
+{
+    row->hl = realloc(row->hl, row->rsize);
+    memset(row->hl, HL_NORMAL, row->rsize);
+
+    for (int i = 0; i < row->rsize; i++)
+    {
+        if (isdigit(row->render[i]))
+            row->hl[i] = HL_NUMBER;
+    }
+}
+
+int editorSyntaxToColor(int hl)
+{
+    switch(hl)
+    {
+        case HL_NUMBER: return 31;
+        default: return 37;
+    }
+}
+
 /*** row operations **/
 
 int editorRowCxToRx(erow *row, int cx)
@@ -300,6 +329,8 @@ void editorUpdateRow(erow *row)
 
     row->render[idx] = '\0';
     row->rsize = idx;
+
+    editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char *s, size_t len)
@@ -317,6 +348,7 @@ void editorInsertRow(int at, char *s, size_t len)
 
     E.row[at].rsize = 0;
     E.row[at].render = NULL;
+    E.row[at].hl = NULL;
     editorUpdateRow(&E.row[at]);
 
     E.numrows++;
@@ -327,6 +359,7 @@ void editorFreeRow(erow *row)
 {
     free(row->render);
     free(row->chars);
+    free(row->hl);
 }
 
 void editorDelRow(int at)
@@ -669,17 +702,34 @@ void editorDrawRows(struct abuf *ab)
             if (len > E.screencols)
                 len = E.screencols;
             char *c = &E.row[filerow].render[E.coloff];
+            unsigned char *hl = &E.row[filerow].hl[E.coloff];
+            int current_color = -1;
+
             for (int j = 0 ; j < len; j ++)
             {
-                if (isdigit(c[j]))
+                if (hl[j] == HL_NORMAL)
                 {
-                    abAppend(ab, "\x1b[31m", 5);
+                    if (current_color != -1)
+                    {
+                        abAppend(ab, "\x1b[39m", 5);
+                        current_color = -1;
+                    }
                     abAppend(ab, &c[j], 1);
-                    abAppend(ab, "\x1b[39m", 5);
                 }
                 else
+                {
+                    int color = editorSyntaxToColor(hl[j]);
+                    if (color != current_color)
+                    {
+                        current_color = color;
+                        char buf[16];
+                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
+                        abAppend(ab, buf, clen);
+                    }
                     abAppend(ab, &c[j], 1);
+                }
             }
+            abAppend(ab, "\x1b[39m", 5);
         }
         //(Erase In Line) erases the part of the line to the right of the cursor
         abAppend(ab, "\x1b[K", 3);
